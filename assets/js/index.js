9d7a11a3a8b5096fc21c52c1ac937e04d01f1ed7
Pedro Pinheiro <pedropereira0604@gmail.com>
2023-02-22 00:38:19 -0300

    Update index.js.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,23 @@
-alert('Pedro ama a Larissa :)')
+var nomeSecreto = 'Larissa';
+var tentativas = 10;
+
+while (tentativas > 0) {
+    var chute = prompt('Tente adivinhar quem é o amor do Pedro!');
+
+    if (chute == nomeSecreto) {
+        alert('Você acertou! O amor do Pedro é ' + nomeSecreto);
+        break;
+    }  else if (chute != nomeSecreto) {
+         tentativas = tentativas - 1;
+         alert('Você errou! Tente novamente. Você possui ' + tentativas + ' tentativas');
+    } else if (tentativas == 5) {
+         tentativas = tentativas - 1;
+         alert('Você está me decepcionado... Ainda restam ' + tentativas + ' tentativas');  
+    } else if (tentativas == 0) {
+         alert('Você excedeu o número de tentativas. Estou muito triste :/');
+    }
+}
+
+if (tentativas == 5) {
+    alert('Você está me decepcionando... Ainda restam ' + tentativas + ' tentativas');
+}
